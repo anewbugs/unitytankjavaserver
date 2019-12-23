8421b22a8eb02bda92ada3a7cc00ecea58091e1a
Wu <876573435@qq.com>
2019-12-23 17:39:09 +0800

    readme

--- src/main/java/com/wu/server/proto/base/MsgName.java
+++ src/main/java/com/wu/server/proto/base/MsgName.java
@@ -1,10 +1,16 @@
 package com.wu.server.proto.base;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MsgName {
     public static final class Battle {
         public static final String MSG_ENTER_BATTLE = "MsgEnterBattle";
         public static final String MSG_BATTLE_RESULT = "MsgBattleResult";
         public static final String MSG_LEAVE_BATTLE = "MsgLeaveBattle";
+
     }
     public static final class Login {
         public static final String MSG_REGISTER = "MsgRegister";
@@ -12,6 +18,8 @@ public class MsgName {
         public static final String MSG_KICK = "MsgKick";
         public static final String MSG_RECONNECT = "MsgReconnect";
         public static final String MSG_OFF_LINE = "MsgOffline"; //掉线消息
+
+
     }
     public static final class User{
         public static final String MSG_GET_ACHIEVE = "MsgGetAchieve";
@@ -42,4 +50,32 @@ public class MsgName {
         public static final String MSG_PONG = "MsgPong";
     }
 
+    private  final HashMap<Class , ArrayList<String>> proto = new HashMap();
+    public static MsgName instance = new MsgName();
+
+    private MsgName(){init();}
+
+    private void init() {
+        Class innerClazz[] = this.getClass().getClasses();
+        for (Class clazz : innerClazz) {
+            ArrayList<String> innProto = new ArrayList<String>();
+           proto.put(clazz,innProto);
+           Field field[] = clazz.getFields();
+            for (Field field1 : field) {
+
+                try {
+                    innProto.add((String) field1.get(clazz));
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        for(Map.Entry entry : instance.proto.entrySet()){
+            System.out.println(entry.getKey());
+        }
+    }
 }
